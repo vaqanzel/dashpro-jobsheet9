eb74801591cd7db07c4068ff0d07b44a95be60e4
vaqanzel <valengrehasta@gmail.com>
2023-10-26 00:35:30 +0700

    ubah statement

--- ArrayBilangan28.java
+++ ArrayBilangan28.java
@@ -1,13 +1,11 @@
 //import java.util.Scanner;
 public class ArrayBilangan28 {
-        public static void main(String[] args) {
-       // Scanner sc = new Scanner(System.in);
-       int[] bil = {12867, 5, 7, 2000000};
-       System.out.println(bil[0]);
-       System.out.println(bil[1]);
-       System.out.println(bil[2]);
-       System.out.println(bil[3]);
-}
+    public static void main(String[] args) {
+            int[] bil = {12867, 5, 7, 2000000};
+            for (int i = 0; i < 4; i++){
+            System.out.println(bil[i]);
+            }
+} 
 
      }
 
