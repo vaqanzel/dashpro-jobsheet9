8e9043fb80d728274834551279c67ead8dd874c2
vaqanzel <valengrehasta@gmail.com>
2023-10-26 00:32:57 +0700

    modifikasi 1

--- ArrayBilangan28.java
+++ ArrayBilangan28.java
@@ -2,17 +2,14 @@
 public class ArrayBilangan28 {
         public static void main(String[] args) {
        // Scanner sc = new Scanner(System.in);
-        int [] bil = new int[4];
-        bil [0] = 5;
-        bil [1] = 13;
-        bil [2] = -7;
-        bil [3] = 17;
-
+       int[] bil = {12867, 5, 7, 2000000};
        System.out.println(bil[0]);
        System.out.println(bil[1]);
        System.out.println(bil[2]);
        System.out.println(bil[3]);
+}
+
      }
 
         
-    }
+    
