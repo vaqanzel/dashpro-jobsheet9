7ccaf389eb9018daf5700421031d3181816bca81
vaqanzel <valengrehasta@gmail.com>
2023-10-26 01:18:48 +0700

    mengubah kondisi pada statement for-loop

--- ArrayBilangan28.java
+++ ArrayBilangan28.java
@@ -2,7 +2,7 @@
 public class ArrayBilangan28 {
     public static void main(String[] args) {
             int[] bil = {12867, 5, 7, 2000000};
-            for (int i = 0; i < 4; i++){
+            for (int i = 0; i <= 4; i++){
             System.out.println(bil[i]);
             }
 } 
