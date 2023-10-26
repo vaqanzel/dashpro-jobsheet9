fb1d7ac24d153ecad9db2654a781b90b7b928c2d
vaqanzel <valengrehasta@gmail.com>
2023-10-26 15:57:15 +0700

    modifikasi pertanyaan 2

--- ArrayNilai28.java
+++ ArrayNilai28.java
@@ -5,12 +5,18 @@ public class ArrayNilai28 {
         Scanner sc = new Scanner (System.in);
         int[] nilaiAkhir = new int[10];
       
-        for (int i =0; i < 10; i++){
+        for (int i =0; i < nilaiAkhir.length; i++){
             System.out.print("Masukan nilai akhir ke-"+i+" : ");
             nilaiAkhir[i] = sc.nextInt();
        }
-        for (int i = 0; i < 10; i++){
-            System.out.println("Nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
+        for (int i = 0; i < nilaiAkhir.length; i++){
+            if (nilaiAkhir[i] > 70){
+               System.out.println("Mahasiswa ke-"+i+" lulus! ");
+            }
+            else {
+                System.out.println("Mahasiswa ke-" +i+ " tidak lulus! ");
+                
+            }
         }
     }
 }
