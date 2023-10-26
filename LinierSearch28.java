2045d5075ec1923fca5b40a79b9d3d2fe0c94784
vaqanzel <valengrehasta@gmail.com>
2023-10-27 06:40:18 +0700

    Percobaan 4

---
+++ LinierSearch28.java
@@ -0,0 +1,15 @@
+public class LinierSearch28 {
+    public static void main(String[] args) {
+        int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
+        int key = 20;
+        int hasil = 0;
+        for (int i = 0; i < arrayInt.length; i++){
+        if (arrayInt[i] == key){
+        hasil = i;
+        break;
+        }
+        }
+        System.out.println("key ada dalam array pada posisi indeks ke-"+hasil);
+        }
+        
+}
