48be79da7cc8719b4ec19a64ca4d26976165ef26
vaqanzel <valengrehasta@gmail.com>
2023-10-27 07:13:04 +0700

    modifikasi 2

--- LinierSearch28.java
+++ LinierSearch28.java
@@ -1,17 +1,20 @@
 import java.util.Scanner;
 public class LinierSearch28 {
     public static void main(String[] args) {
+
        Scanner sc = new Scanner (System.in);
        System.out.print("Masukkan jumlah elemen array: ");
        int n = sc.nextInt();
        int[] arrayInt = new int[n];
+
        for (int i = 0; i < n; i++) {
         System.out.print("Masukkan elemen array ke-" + i + ": ");
         arrayInt[i] = sc.nextInt();
     }
-    System.out.print("Masukkan key yang ingin dicari: ");
+         System.out.print("Masukkan key yang ingin dicari: ");
     int key = sc.nextInt();
     int hasil = -1;
+    
     for (int i = 0; i < arrayInt.length; i++) {
         if (arrayInt[i] == key) {
             hasil = i;
